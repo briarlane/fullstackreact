b721ddaa51fb3cd947cbaaadaf418fef9fa59d3d
Matthew Goff <matt@briarlanegroup.com>
2018-07-13 14:42:12 -0700

    fixed typo

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 // production keys heres
-module.export = {
-    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
-    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+module.exports = {
+  GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+  GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
 };
