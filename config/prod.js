3af143324c1e492fd3320985442dec515ef00cf4
Matthew Goff <matt@briarlanegroup.com>
2018-07-13 14:14:14 -0700

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// production keys here
+module.export = {
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: GOOGLE_CLIENT_SECRET,
+    mongoURI: MONGO_URI,
+    cookieKey: COOKIE_KEY
+};
