098114667c3c79ac435ea59048e652dd711e40ab
Matthew Goff <matt@briarlanegroup.com>
2018-07-18 11:16:25 -0700

    deploy react to heroku

--- models/User.js
+++ models/User.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-userSchema = new Schema ({
-    googleId: String 
+userSchema = new Schema({
+  googleId: String,
+  credits: { type: Number, default: 0 }
 });
 
 mongoose.model('users', userSchema);
